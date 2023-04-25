e245af39f8a4bf3462941faed626148c7e7b68e5
sami <samif3443@gmail.com>
2023-04-25 13:49:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,25 +15,25 @@ import './style.css';
 // }
 // GetShows()
 
+let count = 0;
+
 const GetShows=async ()=>{
  try {
   
-  const listsMovie=document.querySelector('.listsMovie');
+  const listsMovie = document.querySelector('.listsMovie');
   for(let i=1 ;i <=15 ;i++){
    const url = `https://api.tvmaze.com/shows/${i}`
    const res = await fetch(url);
    const shows = await res.json();
-   console.log(res)
-   const Movie =document.createElement('div');
+   const Movie = document.createElement('div');
    Movie.className='Movie'
    Movie.innerHTML =`
    <img src="${shows.image.medium}"
    <div class="ItemLike">
-     <div>${shows.name}</div>
+     <div>${shows.name}  ${i}</div>
      <div class="likeDiv">
-       <button> <i class='fa fa-heart-o green-color'></i></button>
-       <div>
-         3 Likes
+       <button class="reacttoit"> <i class='fa fa-heart-o green-color'></i></button>
+       <div class="dis">likes
        </div>
      </div>
      
@@ -43,9 +43,23 @@ const GetShows=async ()=>{
    `
    listsMovie.appendChild(Movie)
   }
+
+  const likes = document.querySelectorAll('.reacttoit');
+  likes.forEach((elem) => {
+    elem.addEventListener('click', nuoflikes)
+  })
+
+  function nuoflikes() {
+    count += 1;
+    const ll = document.createElement('div')
+    const dis = document.querySelector('.dis');
+    dis.innerHTML =  `<h1>likes ${count}</h1>`
+    console.log('yes');
+  }
   
  }catch(error) {
   console.log("error", error);
  }
 }
+
 GetShows()
