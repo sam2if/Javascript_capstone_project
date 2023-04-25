085f33018d609c6bf858d45553f5e65ff0b6d5dd
sami <samif3443@gmail.com>
2023-04-25 05:36:33 -0700

    Add homapage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import _ from 'lodash';
 import './style.css';
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+
+const url = 'https://api.tvmaze.com/shows'
+
+// const GetShows=async ()=>{
+//  try {
+//   const res = await fetch(url);
+//   const shows = await res.json();
+//   console.log(shows)
+
+//  }catch(error) {
+//   console.log("error", error);
+//  }
+// }
+// GetShows()
+
+const GetShows=async ()=>{
+ try {
+  const listsMovie=document.querySelector('.listsMovie');
+  for(let i=1 ;i <=9 ;i++){
+   const res = await fetch(`"${url}/${[i]}`);
+   const shows = await res.json();
+   console.log(shows)
+   const Movie =document.createElement('div');
+   Movie.className='Movie'
+   Movie.innerHTML =`
+   <img src="${shows[image].medium}"
+   <div class="ItemLike">
+     <div>Movie</div>
+     <div class="likeDiv">
+       <button> <i class='fa fa-heart-o green-color'></i></button>
+       <div>
+         3 Likes
+       </div>
+     </div>
+     
+   </div>
+   <div class="DivBtncomment"><button>comments</button></div>   
+   
+   `
+   listsMovie.appendChild(Movie)
   }
   
-  document.body.appendChild(component());
+ }catch(error) {
+  console.log("error", error);
+ }
+}
+GetShows()
