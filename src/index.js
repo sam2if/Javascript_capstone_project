5aae02f4a61404345368ae66136b6d2339a1c661
massa <zoumaniguimassa12@gmail.com>
2023-04-25 14:13:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 
-
-
-// const GetShows=async ()=>{
-//  try {
-//   const res = await fetch(url);
-//   const shows = await res.json();
-//   console.log(shows)
-
-//  }catch(error) {
-//   console.log("error", error);
-//  }
-// }
-// GetShows()
-
 const GetShows=async ()=>{
  try {
   
-  const listsMovie=document.querySelector('.listsMovie');
+  const listsMovie = document.querySelector('.listsMovie');
   for(let i=1 ;i <=15 ;i++){
    const url = `https://api.tvmaze.com/shows/${i}`
    const res = await fetch(url);
    const shows = await res.json();
    console.log(res)
-   const Movie =document.createElement('div');
+   const Movie = document.createElement('div');
    Movie.className='Movie'
    Movie.innerHTML =`
    <img src="${shows.image.medium}"
    <div class="ItemLike">
-     <div>${shows.name}</div>
+     <div>${shows.name}  ${i}</div>
      <div class="likeDiv">
        <button> <i class='fa fa-heart-o green-color'></i></button>
        <div>
@@ -48,4 +34,5 @@ const GetShows=async ()=>{
   console.log("error", error);
  }
 }
+
 GetShows()
