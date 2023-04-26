a258fb5d8c2027b899b429c8d07fc651d705a950
sami <samif3443@gmail.com>
2023-04-25 17:42:31 -0700

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import _ from 'lodash';
 import './style.css';
 
-
-
 // const GetShows=async ()=>{
 //  try {
 //   const res = await fetch(url);
@@ -15,25 +13,24 @@ import './style.css';
 // }
 // GetShows()
 
+let count = 0;
 const GetShows=async ()=>{
  try {
   
-  const listsMovie=document.querySelector('.listsMovie');
+  const listsMovie = document.querySelector('.listsMovie');
   for(let i=1 ;i <=15 ;i++){
    const url = `https://api.tvmaze.com/shows/${i}`
    const res = await fetch(url);
    const shows = await res.json();
-   console.log(res)
-   const Movie =document.createElement('div');
+   const Movie = document.createElement('div');
    Movie.className='Movie'
    Movie.innerHTML =`
    <img src="${shows.image.medium}"
    <div class="ItemLike">
-     <div>${shows.name}</div>
+     <div>${shows.name}  ${i}</div>
      <div class="likeDiv">
-       <button> <i class='fa fa-heart-o green-color'></i></button>
-       <div>
-         3 Likes
+       <button class="reacttoit"> <i class='fa fa-heart-o green-color'></i></button>
+       <div class="dis">likes
        </div>
      </div>
      
@@ -43,9 +40,40 @@ const GetShows=async ()=>{
    `
    listsMovie.appendChild(Movie)
   }
+
+  let displayLikes = async () => {
+    const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/32UoQxjPo3aTpkPwZxqF/likes/');
+    const data = await res.json();
+    const dis = document.querySelector('.dis');
+    dis.innerHTML =  `<h1>likes ${data[0].likes += 1}</h1>`
+  }
+
+  const likes = document.querySelectorAll('.reacttoit');
+  likes.forEach((elem) => {
+    elem.addEventListener('click', displayLikes)
+  })
+
+ 
   
  }catch(error) {
   console.log("error", error);
  }
 }
+
 GetShows()
+
+let getLikes = async () => {
+  try {
+    const items = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/32UoQxjPo3aTpkPwZxqF/likes/', {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: 'item1'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });  
+  }catch(error) {
+    console.log('error', error);
+  }
+}
