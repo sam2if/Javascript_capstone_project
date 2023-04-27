430f39fc7b16b67e9faca7ad19104ff9cc265265
sami <samif3443@gmail.com>
2023-04-27 06:26:33 -0700

    Added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-// import _, { get } from 'lodash';
 import './style.css';
-import { commentPopup, Getcomment } from './Modules/Popup.js';
 
-
-import { allLikes } from './module/forlikes';
+import { allLikes } from './module/forlikes.js';
 
 const GetShows = async () => {
   try {
@@ -23,112 +20,15 @@ const GetShows = async () => {
      <div class="likeDiv">
        <button class="reacttoit${i}"> <i class='fa fa-heart-o green-color'></i></button>
        <div class="dis${i}">likes
-=======
-const GetShows = async () => {
-  try {
-    const listsMovie = document.querySelector('.listsMovie');
-    for (let i = 1; i <= 15; i++) {
-      const url = `https://api.tvmaze.com/shows/${i}`;
-      const res = await fetch(url);
-      const shows = await res.json();
-
-      const Movie = document.createElement('div');
-      const Position = document.createElement('div');
-      Position.className = 'Position';
-      Position.innerHTML = `
-   <div class="FramePopup">
-      <div class="timesDiv"> <button class="fa-timesBtn"><i class='fa fa-times'></i></button></div>
-       <div class="popupImage">
-         <img src="${shows.image.original}" alt="" srcset="" class="PopupImage">
-       
-       </div>
-       <div class="Popupname">${shows.name}</div>
-     
-       <div class="specifits">
-         <div>
-           <div>Language :${shows.language
-}</div>
-           <div>Genres :${shows.genres.toString()
-}</div>
-         </div>
-         <div>
-           <div>Premiered
-           :${shows.premiered
-
-}</div>
-           <div>Ended :${shows.ended
-
-}</div>
-
-         </div>
-       </div>
-       <h2>summary</h2>
-       <div class="sumary">${shows.summary
-}</div>
-       <div class="placeComment"></div>
-       <form class="form" action="">
-         <input class="inputText1" type="text">
-        <textarea class="inputText2" name="" id="" cols="30" rows="10"></textarea>
-        <div class="DivcommentPOPup"> <button class="commentPOPup" type="button">Comment</button></div>
-       </form>
-     </div>
-   `;
-
-      Movie.className = 'Movie';
-      Movie.innerHTML = `
-   <img src="${shows.image.medium}">
-   <div class="ItemLike">
-     <div class="showName">${shows.name}  ${i}</div>
-     <div class="likeDiv">
-       <button class="Btnfa-heart-o"> <i class='fa fa-heart-o green-color'></i></button>
-       <div class="NbrLike">
-
        </div>
      </div>
      
    </div>
-
    <div class="DivBtncomment"><button>comments</button></div>   
    
    `;
       listsMovie.appendChild(Movie);
     }
-
-   <div class="DivBtncomment"> <button class="Btncomment" type="button">comments</button> </div>  
-   `;
-      Movie.appendChild(Position);
-      // listsMovie.appendChild(Position);
-      listsMovie.appendChild(Movie);
-    }commentPopup();
-  
-    const ShowPopup = () => {
-      const Btncomment1 = document.querySelectorAll('.Btncomment');
-      const arrBtncomment = Array.from(Btncomment1);
-      arrBtncomment.forEach((element) => {
-        element.addEventListener('click', () => {
-          const pop = element.parentElement.parentElement.querySelector('.Position');
-          pop.style.display = 'flex';
-          const op = element.parentElement.parentElement.querySelector('.Position').querySelector('.FramePopup').querySelector('.placeComment');
-          
-          Getcomment(op);
-        });
-      });
-    };
-    ShowPopup();
-    const ClossePopup = () => {
-      let Bntcloss = document.querySelectorAll('.fa-timesBtn');
-      let arrBntcloss = Array.from(Bntcloss);
-      arrBntcloss.forEach((element) => {
-        element.addEventListener('click', () => {
-          element.parentElement.parentElement.parentElement.querySelector('.FramePopup').style.display = 'none';
-          element.style.display = 'none';
-           Bntcloss = document.querySelectorAll('.fa-timesBtn');
-           arrBntcloss = Array.from(Bntcloss);
-        });
-      });
-    };
-    ClossePopup();
-
   } catch (error) {
     console.log('error', error);
   }
@@ -152,4 +52,3 @@ for (let i = 0; i <= 15; i += 1) {
   };
   displayLikes();
 }
-
